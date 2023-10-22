62b544e35ebb03a61efc1205a7e6f6f0e67abab6
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-10-22 11:10:19 +0200

    lab2: add tryb l

--- lab2/pracadomowa2/pracadomowa2.py
+++ lab2/pracadomowa2/pracadomowa2.py
@@ -76,23 +76,38 @@ def wypelnij_dopunktu(h,w,m,n):
     return tab_show
 
 #----------------------------------------------------------
-#funkcja pokoloruj_odleglosciami tworzy koncentryczne gradienty 
+#funkcja rysuj_kola tworzy okregi 
 #rozchodzace sie od punktu (n,m).
-#ich gestosc mozemy regulowac parametrem gestosc
+#ich gestosc mozemy regulowac parametrem grubosc
 
-def pokoloruj_odleglosciami(h,w,m,n, gestosc=1):
+def rysuj_kola(h,w,m,n, grubosc):
     tab1 = np.ones((h,w), dtype=np.uint8)
     for i in range(h):
         for j in range(w):
-            tab1[i,j] = (math.sqrt((i-n)**2 + (j-m)**2)%gestosc)
+            tab1[i,j] = math.sqrt((i-n)**2 + (j-m)**2)%grubosc
 
     tab2 = tab1.astype(np.bool_)
     tab_show = Image.fromarray(tab2)
     return tab_show
 
 #----------------------------------------------------------
+#funkcja rysuj_kola_l tworzy koncentryczne gradienty 
+#rozchodzace sie od punktu (n,m).
+#ich gestosc mozemy regulowac parametrem gestosc
 
-#
+def rysuj_kola_l(h,w,m,n, gestosc=1):
+    tab1 = np.ones((h,w), dtype=np.uint8)
+    for i in range(h):
+        for j in range(w):
+            tab1[i,j] = (math.sqrt((i-n)**2 + (j-m)**2)*gestosc)%256
+
+    tab_show = Image.fromarray(tab1)
+    return tab_show
+
+#----------------------------------------------------------
+#----------------------------------------------------------
+#----------------------------------------------------------
+
 #ramka = rysuj_ramke(obrazek, 5)
 #ramka.save("ramka_5.bmp")
 #
@@ -107,11 +122,15 @@ def pokoloruj_odleglosciami(h,w,m,n, gestosc=1):
 #
 #ramka = wypelnij_dopunktu(320,480,100,50)
 #ramka.save("obraz3.bmp")
-#
-#funkcja pokoloruj_odleglosciami tworzy koncentryczne gradienty 
-#rozchodzace sie od punktu (m,n).
-#ich gestosc mozemy regulowac parametrem gestosc
 
-ramka = pokoloruj_odleglosciami(320,480,100,50,10) 
-ramka.save("obraz4.bmp")
+#funkcja rysuj_kola tworzy okregi 
+#rozchodzace sie od punktu (m,n).
+#ich gestosc mozemy regulowac parametrem grubosc
+
+#ramka = rysuj_kola(320,480,100,50,44) 
+#ramka.save("obraz4.bmp")
+
+
+
+ramka = rysuj_kola_l(320,480,100,50,3) 
 ramka.show()
