e6887e753082147b7425276e5c1a79cc9b1063ff
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-10-22 11:02:33 +0200

    lab2: funkcje w trybie 1

--- lab2/pracadomowa2/pracadomowa2.py
+++ lab2/pracadomowa2/pracadomowa2.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import numpy as np
+import math
+
 
 obrazek = Image.open ("inicjaly.bmp")
 
+#----------------------------------------------------------
 def rysuj_ramke(obraz, grub):
     tab_obrazu = np.asarray(obraz)
     h, w = tab_obrazu.shape
@@ -42,11 +45,9 @@ def rysuj_same_ramki(h, w, grub):
                 elif j in range(w-grub-x, w-x):
                     tab1[i,j] = 0
 
-
     tab2 = tab1.astype(np.bool_)
     tab_show = Image.fromarray(tab2)
     return tab_show
-
 
 #----------------------------------------------------------
 def rysuj_pionowe(h,w, grub):
@@ -61,16 +62,56 @@ def rysuj_pionowe(h,w, grub):
     return tab_show
 
 #----------------------------------------------------------
+def wypelnij_dopunktu(h,w,m,n):
+    tab1 = np.ones((h,w), dtype=np.uint8)
+    for i in range(h):
+        for j in range(w):
+            if (i<m and j<n):
+                tab1[i,j] = 0
+            elif (i>m and j>n):
+                tab1[i,j] = 0
 
+    tab2 = tab1.astype(np.bool_)
+    tab_show = Image.fromarray(tab2)
+    return tab_show
+
+#----------------------------------------------------------
+#funkcja pokoloruj_odleglosciami tworzy koncentryczne gradienty 
+#rozchodzace sie od punktu (n,m).
+#ich gestosc mozemy regulowac parametrem gestosc
+
+def pokoloruj_odleglosciami(h,w,m,n, gestosc=1):
+    tab1 = np.ones((h,w), dtype=np.uint8)
+    for i in range(h):
+        for j in range(w):
+            tab1[i,j] = (math.sqrt((i-n)**2 + (j-m)**2)%gestosc)
+
+    tab2 = tab1.astype(np.bool_)
+    tab_show = Image.fromarray(tab2)
+    return tab_show
+
+#----------------------------------------------------------
+
+#
 #ramka = rysuj_ramke(obrazek, 5)
 #ramka.save("ramka_5.bmp")
 #
 #ramka = rysuj_ramke(obrazek, 10)
 #ramka.save("ramka_10.bmp")
-
-#ramka = rysuj_same_ramki(100,150,4)
+#
+#ramka = rysuj_same_ramki(320,480,10)
 #ramka.save("obraz1.bmp")
+#
+#ramka = rysuj_pionowe(320,480,10)
+#ramka.save("obraz2.bmp")
+#
+#ramka = wypelnij_dopunktu(320,480,100,50)
+#ramka.save("obraz3.bmp")
+#
+#funkcja pokoloruj_odleglosciami tworzy koncentryczne gradienty 
+#rozchodzace sie od punktu (m,n).
+#ich gestosc mozemy regulowac parametrem gestosc
 
-
-ramka = rysuj_pionowe(100,150,4)
+ramka = pokoloruj_odleglosciami(320,480,100,50,10) 
+ramka.save("obraz4.bmp")
 ramka.show()
