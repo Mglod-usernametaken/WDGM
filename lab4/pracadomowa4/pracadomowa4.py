85c7215dd0b2ec7509bf7762699ab40902982a10
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-11-05 14:37:40 +0100

    lab4: add zadanie 1+2 całe

---
+++ lab4/pracadomowa4/pracadomowa4.py
@@ -0,0 +1,96 @@
+from PIL import Image
+import numpy as np
+from PIL import ImageChops
+from PIL import ImageStat as stat
+import matplotlib.pyplot as plt
+from random import randint
+
+#----------------------------------------------------
+def statystyki(im):
+    s = stat.Stat(im)
+    print("extrema ", s.extrema)  # max i min
+    print("count ", s.count)  # zlicza
+    print("mean ", s.mean)  # srednia
+    print("median ", s.median)  # mediana
+    print("stddev ", s.stddev)  # odchylenie standardowe
+
+#----------------------------------------------------
+def zlicz_roznice_srednia_RGB(obraz, wsp): # wsp - współczynnik określający dokładność oceny
+    t_obraz = np.asarray(obraz)
+    h, w, d = t_obraz.shape
+    zlicz = 0
+    for i in range(h):
+        for j in range(w):
+                if np.mean(t_obraz[i, j, :]) > wsp:    
+                    zlicz = zlicz + 1 
+    procent = zlicz/(h*w)
+    return zlicz, procent
+
+#----------------------------------------------------
+def zlicz_roznice_suma_RGB(obraz, wsp): # wsp - współczynnik określający dokładność oceny
+    t_obraz = np.asarray(obraz)
+    h, w, d = t_obraz.shape
+    zlicz = 0
+    for i in range(h):
+        for j in range(w):
+                if sum(t_obraz[i, j, :]) > wsp: # poniżej równoważne sformułowania tego warunku
+                # if (t_obraz[i, j, 0] + t_obraz[i, j, 1] + t_obraz[i, j, 2]) > wsp:
+                # if t_obraz[i, j, 0] > wsp or  t_obraz[i, j, 1] > wsp or t_obraz[i, j, 2] > wsp: 
+                    zlicz = zlicz + 1 
+    procent = zlicz/(h*w)
+    return zlicz, procent
+
+#----------------------------------------------------
+def generuj_histogram(im):
+    hist = im.histogram()
+    p = 0
+    print(hist[p])
+    print(hist[256 + p])
+    print(hist[2*256 + p])
+    plt.bar(range(256), hist[:256], color='r', alpha=0.5)
+    plt.bar(range(256), hist[256:2 * 256], color='g', alpha=0.4)
+    plt.bar(range(256), hist[2 * 256:], color='b', alpha=0.3)
+    plt.show()
+    #plt.savefig("histogram1.png")
+
+# im3jpg = Image.open("im3.jpg")
+# im3png = Image.open("im3.png")
+# 
+# diff_3 = ImageChops.difference(im3png,im3jpg)
+# diff_3.show()
+# diff_3.save("diff.png")
+
+diff = Image.open("diff.png")
+statystyki(diff)
+
+# generuj_histogram(diff)
+# #generuj_histogram(im3jpg)
+# 
+# zlicz, procent = zlicz_roznice_srednia_RGB(diff, 5)
+# print('liczba niepasujących pikseli = ' , zlicz)
+# print('procent niepasujących pikseli = ' , procent)
+# 
+# zlicz, procent = zlicz_roznice_suma_RGB(diff, 5)
+# print('liczba niepasujących pikseli = ' , zlicz)
+# print('procent niepasujących pikseli = ' , procent)
+# 
+
+
+obraz = Image.open("shrimp.jpg")
+obraz.save("obraz1.jpg")
+obraz1 = Image.open("obraz1.jpg")
+obraz.save("obraz2.jpg")
+obraz2 = Image.open("obraz2.jpg")
+obraz.save("obraz3.jpg")
+obraz3 = Image.open("obraz3.jpg")
+obraz.save("obraz4.jpg")
+obraz4 = Image.open("obraz4.jpg")
+obraz.save("obraz5.jpg")
+obraz5 = Image.open("obraz5.jpg")
+
+# obraz5.show()
+diff5 = ImageChops.difference(obraz, obraz5)
+# diff5.show()
+generuj_histogram(diff5)
+ststystyki(diff5)
+
