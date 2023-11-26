fe97bc1a4d62b465fe0326477c5dec442e01175c
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-11-26 13:01:23 +0100

    lab5: initial commit

---
+++ lab5/pracadomowa5/pracadomowa5.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+shrimp = Image.open("shrimp.jpg")
+mg = Image.open("inicjaly.bmp")
+
+def zakres(h, w):
+    return[(i, j) for i in range(w) for j in range(h)]
+
+#-------------------------------------------------------------------
+def wstaw_inicjaly(obrazek, inicjal, m, n, r, g, b):
+    obraz = obrazek.load()
+    ini = np.asarray(inicjal)
+    w, h  = obrazek.size
+    wi, hi = inicjal.size
+    
+    for i, j in zakres(wi, hi):
+        if 0 == ini[i,j]:
+            obraz[i,j] = (r, g, b)
+    obraz.show()
+    return obraz
+
+
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+wstaw_inicjaly(shrimp, mg, 100,100,255,0,255)
+
