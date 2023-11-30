251ac304bea665dcadc618c0cd8139bfb182aa68
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-12-01 00:19:28 +0100

    lab5: add log function initial drawings

--- lab5/pracadomowa5/pracadomowa5.py
+++ lab5/pracadomowa5/pracadomowa5.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-shrimp = Image.open("shrimp.jpg")
+shrimp = Image.open("shrimp_small.jpg")
 mg = Image.open("inicjaly.bmp")
 
 def zakres(w, h):
@@ -22,10 +22,33 @@ def wstaw_inicjaly(obrazek, inicjal, m, n, r, g, b):
     obrazek.show()
     return obraz
 
-
-
-
 #-------------------------------------------------------------------
+def filtruj_inicjaly(obrazek, inicjal, m, n):
+    obraz = obrazek.load()
+    ini = np.asarray(inicjal)
+    w, h  = obrazek.size
+    hi, wi = inicjal.size
+    
+    for i, j in zakres(wi, hi):
+        if 0 == ini[i,j]:
+            (r,g,b) = obraz[j+m,i+n]
+            r = int(255* np.log(1+(r/255)))
+            g = int(255* np.log(1+(g/255)))
+            b = int(255* np.log(1+(b/255)))
+          #  r = 255 
+          #  g = 255 
+          #  b = 255 
+            obraz[j+m,i+n] = (r,g,b)
+    obrazek.show()
+    return obraz
 #-------------------------------------------------------------------
-wstaw_inicjaly(shrimp, mg, 100,100,255,0,255)
-
+def transformacja_logarytmiczna(obrazek):
+    obraz = obrazek.load()
+    w,h = obrazek.size
+    for i,j in zakres(w,h):
+        for k in obraz[i,j]:
+            k = 255* np.log(1+k/255)
+#-------------------------------------------------------------------
+# obrazek1 = wstaw_inicjaly(shrimp, mg,1820,1230, 255,0,255)
+# obraz1.save("obraz1.png")
+obrazek1 = filtruj_inicjaly(shrimp, mg,200,200)
