609b6d8c78ae27bb6026723d63d43583564995fa
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-12-06 22:02:40 +0100

    lab5: finish task3

--- lab5/pracadomowa5/pracadomowa5.py
+++ lab5/pracadomowa5/pracadomowa5.py
@@ -41,6 +41,7 @@ def filtruj_inicjaly(obrazek, inicjal, m, n, factor):
             obraz[j+m,i+n] = (r,g,b)
     obrazek.show()
     return obrazek
+
 #-------------------------------------------------------------------
 def transformacja_logarytmiczna(obrazek):
     obraz = obrazek.load()
@@ -48,10 +49,49 @@ def transformacja_logarytmiczna(obrazek):
     for i,j in zakres(w,h):
         for k in obraz[i,j]:
             k = 255* np.log(1+k/255)
+
 #-------------------------------------------------------------------
+def rysuj_kwadrat_srednia(obraz, m,n,k):
+    if 0 == k%2:
+        print("nieprawidłowa wartość K w rysuj_kwadrat_srednia. pomijam.")
+        return 1
+    obrazek = obraz.load()
+    zakres = int((k-1)/2)
+    print("zakres = ",zakres)
+    red = green = blue = 255
+    print("green = ",green)
+    
+    for i in range (m-zakres, m+zakres):
+        for j in range (n-zakres, n+zakres):
+            (r,g,b) = obrazek[i, j]
+            if red > r:
+                red = r
+            if green > g:
+                green = g
+            if blue > b:
+                blue = b
+
+    for i in range (m-zakres, m+zakres):
+        for j in range (n-zakres, n+zakres):
+            obrazek[i,j] = (red,green,blue)
+    print("red = ",red)
+    print("green = ",green)
+    print("blue = ",blue)
+    obraz.show()
+    return obraz
+
+#-------------------------------------------------------------------
+
+#-------------------------------------------------------------------
+
+
 # obrazek1 = wstaw_inicjaly(shrimp, mg,1820,1230, 255,0,255)
 # obrazek1.save("obraz1.png")
 # obrazek2 = filtruj_inicjaly(shrimp, mg,200,200, 255)
 # obrazek2.save("obraz2.png")
 # obrazek2 = filtruj_inicjaly(shrimp, mg,200,200, 125)
 # obrazek2.save("obraz2-enhanced.png")
+obrazek3 = rysuj_kwadrat_srednia(shrimp, 200,200,51)
+obrazek31 = rysuj_kwadrat_srednia(obrazek3, 369,69,21)
+obrazek32 = rysuj_kwadrat_srednia(obrazek31, 575,75,77)
+obrazek32.save("obraz3.png")
