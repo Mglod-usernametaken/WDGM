ccecd5540ab95cf3a18902c7e88d6a0bad39690b
Mglod-usernametaken <mastrwloczykij@gmail.com>
2023-12-26 13:57:29 +0100

    lab6: add scaling

--- lab6/pracadomowa6/pracadomowa6.py
+++ lab6/pracadomowa6/pracadomowa6.py
@@ -125,91 +125,148 @@ shrimp = Image.open("shrimp.jpg")
 # zadanie 4
 #--------------------------------------------------
 
-
-toof = Image.open("zeby.png")
-print(toof.mode)
-teef = toof.convert('L')
-# teef.show()
-teefqualized = ImageOps.equalize(teef, mask = None)
-# teefqualized.show() 
-teefqualized.save("equalized.png")
-
-#--------------------------------------------------
-
-teef_detail = teef.filter(ImageFilter.DETAIL)
-teef_sharpen = teef.filter(ImageFilter.SHARPEN)
-teef_contour = teef.filter(ImageFilter.CONTOUR)
-
-plt.subplot(2,3,1)
-plt.title("zeby format L")
-plt.imshow(teef, "gray")
-plt.axis('off')
-plt.subplot(2,3,2)
-plt.title("zeby_equalized")
-plt.imshow(teefqualized, "gray")
-plt.axis('off')
-plt.subplot(2,3,3)
-plt.title("DETAIL")
-plt.imshow(teef_detail, "gray")
-plt.axis('off')
-plt.subplot(2,3,4)
-plt.title("SHARPEN")
-plt.imshow(teef_sharpen, "gray")
-plt.axis('off')
-plt.subplot(2,3,5)
-plt.title("CONTOUR")
-plt.imshow(teef_contour, "gray")
-plt.axis('off')
-plt.subplots_adjust(wspace=0.05, hspace=0.15)
-plt.savefig("filtry.png")
+# 
+# toof = Image.open("zeby.png")
+# print(toof.mode)
+# teef = toof.convert('L')
+# # teef.show()
+# teefqualized = ImageOps.equalize(teef, mask = None)
+# # teefqualized.show() 
+# teefqualized.save("equalized.png")
+# 
+# #--------------------------------------------------
+# 
+# teef_detail = teef.filter(ImageFilter.DETAIL)
+# teef_sharpen = teef.filter(ImageFilter.SHARPEN)
+# teef_contour = teef.filter(ImageFilter.CONTOUR)
+# 
+# plt.subplot(2,3,1)
+# plt.title("zeby format L")
+# plt.imshow(teef, "gray")
+# plt.axis('off')
+# plt.subplot(2,3,2)
+# plt.title("zeby_equalized")
+# plt.imshow(teefqualized, "gray")
+# plt.axis('off')
+# plt.subplot(2,3,3)
+# plt.title("DETAIL")
+# plt.imshow(teef_detail, "gray")
+# plt.axis('off')
+# plt.subplot(2,3,4)
+# plt.title("SHARPEN")
+# plt.imshow(teef_sharpen, "gray")
+# plt.axis('off')
+# plt.subplot(2,3,5)
+# plt.title("CONTOUR")
+# plt.imshow(teef_contour, "gray")
+# plt.axis('off')
+# plt.subplots_adjust(wspace=0.05, hspace=0.15)
+# plt.savefig("filtry.png")
+# # plt.show()
+# 
+# #--------------------------------------------------
+# def get_histogram(im, name):
+#     plt.clf()
+#     hist = im.histogram()
+#     p = 0
+#     print(hist[p])
+#     plt.bar(range(256), hist[:])
+#     plt.savefig(f"{name}.png")
+# #--------------------------------------------------
+# histo_teef = get_histogram(teef,"histo_teef")
+# histo_teefqualized = get_histogram(teefqualized, "histo_teefqualized")
+# histo_teef_detail =  get_histogram(teef_detail, "histo_teef_detail")
+# histo_teef_sharpen = get_histogram(teef_sharpen, "histo_teef_sharpen")
+# histo_teef_contour = get_histogram(teef_contour, "histo_teef_contour")
+# 
+# plt.clf()
+# plt.subplot(2,3,1)
+# plt.title("zeby format L")
+# histo_teef= Image.open("histo_teef.png")
+# plt.imshow(histo_teef)
+# plt.axis('off')
+# 
+# plt.subplot(2,3,2)
+# plt.title("zeby_equalized")
+# histo_teefqualized= Image.open("histo_teefqualized.png")
+# plt.imshow(histo_teefqualized)
+# plt.axis('off')
+# 
+# plt.subplot(2,3,3)
+# plt.title("DETAIL")
+# histo_teef_detail= Image.open("histo_teef_detail.png")
+# plt.imshow(histo_teef_detail)
+# plt.axis('off')
+# 
+# plt.subplot(2,3,4)
+# plt.title("SHARPEN")
+# histo_teef_sharpen= Image.open("histo_teef_sharpen.png")
+# plt.imshow(histo_teef_sharpen)
+# plt.axis('off')
+# 
+# plt.subplot(2,3,5)
+# plt.title("CONTOUR")
+# histo_teef_contour = Image.open("histo_teef_contour.png")
+# plt.imshow(histo_teef_contour)
+# plt.axis('off')
+# 
+# plt.subplots_adjust(wspace=0.05, hspace=0.15)
+# plt.savefig("histogramy.png")
 # plt.show()
 
 #--------------------------------------------------
-def get_histogram(im, name):
-    plt.clf()
-    hist = im.histogram()
-    p = 0
-    print(hist[p])
-    plt.bar(range(256), hist[:])
-    plt.savefig(f"{name}.png")
+s_w = 0.15
+s_h = 0.27
+# h,w = shrimp.size
+# plt.clf()
+# resample_method =['NEAREST','LANCZOS','BILINEAR','BICUBIC','BOX','HAMMING']
+# shrimp_R = shrimp.resize((int(w*s_w), int(h*s_h)), 0) 
+# plt.figure(figsize=(20, 16))
+# i=1
+# for t in range(6):
+#     file_name = "resample_"+ str(resample_method[t])
+#     im_r = shrimp.resize((int(w*s_w), int(h*s_h)), t)
+#     plt.subplot(6, 2, i)
+#     plt.title(str(file_name))
+#     plt.imshow(im_r)
+#     plt.axis('off')
+#     i +=1
+#     diff=ImageChops.difference(shrimp_R,im_r)
+#     plt.subplot(6, 2, i)
+#     plt.title('roznica ' + file_name)
+#     plt.imshow(diff)
+#     plt.axis('off')
+#     i +=1
+# 
+# plt.savefig("fig5.png")
+
 #--------------------------------------------------
-histo_teef = get_histogram(teef,"histo_teef")
-histo_teefqualized = get_histogram(teefqualized, "histo_teefqualized")
-histo_teef_detail =  get_histogram(teef_detail, "histo_teef_detail")
-histo_teef_sharpen = get_histogram(teef_sharpen, "histo_teef_sharpen")
-histo_teef_contour = get_histogram(teef_contour, "histo_teef_contour")
-
+h,w = shrimp.size
 plt.clf()
-plt.subplot(2,3,1)
-plt.title("zeby format L")
-histo_teef= Image.open("histo_teef.png")
-plt.imshow(histo_teef)
+shrimp_R = shrimp.resize((int(w*s_w), int(h*s_h)), 3) 
+shrimp_B = shrimp_R.resize((1920,1280),3)
+
+# shrimp_R.show()
+# shrimp_B.show()
+
+diff=ImageChops.difference(shrimp,shrimp_B)
+# diff.show()
+
+plt.subplot(1,3,1)
+plt.title("oryginał")
+plt.imshow(shrimp)
 plt.axis('off')
 
-plt.subplot(2,3,2)
-plt.title("zeby_equalized")
-histo_teefqualized= Image.open("histo_teefqualized.png")
-plt.imshow(histo_teefqualized)
+plt.subplot(1,3,2)
+plt.title("po skalowaniu")
+plt.imshow(shrimp_B)
 plt.axis('off')
 
-plt.subplot(2,3,3)
-plt.title("DETAIL")
-histo_teef_detail= Image.open("histo_teef_detail.png")
-plt.imshow(histo_teef_detail)
-plt.axis('off')
-
-plt.subplot(2,3,4)
-plt.title("SHARPEN")
-histo_teef_sharpen= Image.open("histo_teef_sharpen.png")
-plt.imshow(histo_teef_sharpen)
-plt.axis('off')
-
-plt.subplot(2,3,5)
-plt.title("CONTOUR")
-histo_teef_contour = Image.open("histo_teef_contour.png")
-plt.imshow(histo_teef_contour)
+plt.subplot(1,3,3)
+plt.title("diff")
+plt.imshow(diff)
 plt.axis('off')
 
 plt.subplots_adjust(wspace=0.05, hspace=0.15)
-plt.savefig("histogramy.png")
+plt.savefig("plot6.png")
 plt.show()
